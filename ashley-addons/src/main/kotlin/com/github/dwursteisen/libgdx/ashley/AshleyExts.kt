cb26321809dd95f96640f925cf172f09d3c639ea
David <david.wursteisen+git@gmail.com>
2017-09-02 11:05:16 +0200

    add helper method on Engine

--- ashley-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ashley/AshleyExts.kt
+++ ashley-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ashley/AshleyExts.kt
@@ -1,6 +1,12 @@
 package com.github.dwursteisen.libgdx.ashley
 
-import com.badlogic.ashley.core.*
+import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.ComponentMapper
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.ashley.core.Family
+import com.badlogic.ashley.core.PooledEngine
 
 inline fun <reified T : Component> EntitySystem.get(): ComponentMapper<T> = ComponentMapper.getFor(T::class.java)
 
@@ -8,12 +14,15 @@ inline operator fun <reified T : Component> Entity.get(mapper: ComponentMapper<T
 
 inline fun <reified T : Component> Entity.getNullable(mapper: ComponentMapper<T>): T? = mapper.get(this)
 
-inline fun <reified T : Component> PooledEngine.createComponent() : T = this.createComponent(T::class.java)
+inline fun <reified T : Component> PooledEngine.createComponent(): T = this.createComponent(T::class.java)
 
-inline fun <reified T : Component> PooledEngine.createComponentWith(block: T.() -> Unit) : T {
+inline fun <reified T : Component> PooledEngine.createComponentWith(block: T.() -> Unit): T {
     val component = this.createComponent(T::class.java)
     block.invoke(component)
     return component
 }
+
+fun Engine.entity(component: Class<out Component>): Entity = this.getEntitiesFor(Family.all(component).get()).first()
+
 
 
