8ee621262809858c56f5adf257c51d0cf0189143
David <david.wursteisen+git@gmail.com>
2019-01-08 01:13:51 +0100

    ServiceLocator: Create default instance if service not found

--- core-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ServiceLocator.kt
+++ core-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ServiceLocator.kt
@@ -3,7 +3,7 @@ package com.github.dwursteisen.libgdx
 import kotlin.reflect.KProperty
 
 
-inline fun <reified T> locate() : Locator<T> = Locator(T::class.java)
+inline fun <reified T> locate(): Locator<T> = Locator(T::class.java)
 
 class Locator<T>(private val clazz: Class<T>) {
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
@@ -15,16 +15,33 @@ object ServiceLocator {
 
     private var cache: Map<Class<out Any>, Any> = emptyMap()
 
-    operator fun <T> get(clazz: Class<T>): T {
-        val result = cache[clazz] ?: TODO("Unknow service register with the class $clazz")
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T> get(clazz: Class<out T>): T {
+        val result = cache[clazz] ?: tryCreateInstance(clazz) ?: locatingError(clazz)
         return result as T
-   }
+    }
 
-   inline fun <reified T> locate(): T {
-       return this[T::class.java]
-   }
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> tryCreateInstance(clazz: Class<out T>): T? {
+        val emptyConstructor = clazz.constructors.singleOrNull { it.parameterCount == 0 }
+        emptyConstructor ?: return null
 
-   fun register(instance: Any, clazz: Class<out Any>) {
+        val newInstance = emptyConstructor.newInstance()
+        register(newInstance, clazz)
+        return newInstance as T
+
+    }
+
+    private fun <T> locatingError(clazz: Class<T>): Nothing {
+        TODO("Impossible to locate any registered class of type $clazz or to create a default instance of it.")
+    }
+
+
+    inline fun <reified T> locate(): T {
+        return this[T::class.java]
+    }
+
+    fun register(instance: Any, clazz: Class<out Any>) {
         cache += clazz to instance
-   }
+    }
 }
