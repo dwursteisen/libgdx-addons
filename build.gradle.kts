a64410480eac70357319b5c961064a56eaebbb39
David Wursteisen <david.wursteisen+git@gmail.com>
2019-08-18 18:22:19 +0200

    Add documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,24 @@
 import internal.ConfigurationPlugin
+import org.asciidoctor.gradle.AsciidoctorTask
 
 plugins {
     kotlin
+    id("org.asciidoctor.convert") version "2.3.0"
 }
+
+tasks {
+    create("asciidoctor-media", Copy::class) {
+        from(file("src/docs/asciidoc/media"))
+        destinationDir = file("docs/media")
+    }
+
+    "asciidoctor"(AsciidoctorTask::class) {
+        dependsOn("asciidoctor-media")
+        outputDir(file("docs"))
+        separateOutputDirs = false
+    }
+}
+
 allprojects {
 
     apply { plugin(ConfigurationPlugin::class) }
