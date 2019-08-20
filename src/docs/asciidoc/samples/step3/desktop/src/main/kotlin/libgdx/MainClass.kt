a862fdd94f75328e598cb6b2f1b7316d6085c720
David Wursteisen <david.wursteisen+git@gmail.com>
2019-08-20 23:20:52 +0200

    add step3 into source code for documentation

---
+++ src/docs/asciidoc/samples/step3/desktop/src/main/kotlin/libgdx/MainClass.kt
@@ -0,0 +1,16 @@
+package libgdx
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+
+// tag::body[]
+object MainClass {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        LwjglApplication(MyGame(), LwjglApplicationConfiguration().apply {
+            width = 600
+            height = 600
+        })
+    }
+}
+// end::body[]
